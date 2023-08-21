27dbd6ecc7296e11baa6724fc932215c84579c25
lucas-palmeida <lucaspacheco95@gmail.com>
2023-08-20 23:56:37 -0300

    feat: implements user dto, signup post enpoint and tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SignUpUserDTO } from './dto/user.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post("/sign-up")
+  @HttpCode(HttpStatus.OK)
+  signUp(@Body() body: SignUpUserDTO) {
+    try {
+      return this.appService.signUp(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Tweet } from './entities/tweet.entity';
+import { SignUpUserDTO } from './dto/user.dto';
 
 @Injectable()
 export class AppService {
+  private users: User[];
+  private tweets: Tweet[];
+
+  constructor() {
+    this.users = [];
+    this.tweets = [];
+    // Fazer a instanciação dos users e tweets dentro dos arrays;
+  }
+
+  signUp(body: SignUpUserDTO) {
+    const user = new User(body.username, body.avatar);
+    return this.users.push(user);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
